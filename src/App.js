102a09ff204a75d7d78df319063b0058803bcfa4
abrunello <brunello@bludata.com>
2022-01-12 15:03:11 +0100

    defining useHtpp custom hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -9,11 +10,12 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const fetchTasks = async (taskText) => {
+
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://react-udemy-course-custom-hook-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
