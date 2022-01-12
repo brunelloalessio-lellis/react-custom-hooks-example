10d6fe01390c590304bfa8fd6f1d77fc8a0d3603
abrunello <brunello@bludata.com>
2022-01-12 15:47:13 +0100

    using custom hook inside more than one component

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,25 +1,29 @@
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
-import useHttp from '../../hooks/use-http';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
   const { isLoading, error, sendRequest: addNewTask } = useHttp();
-  
+
+  const createTask = (responseData, taskText) => {
+    const generatedId = responseData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
   const enterTaskHandler = async (taskText) => {
-
-    addNewTask({
-      url:'https://react-udemy-course-custom-hook-default-rtdb.firebaseio.com/tasks.json',
-      method: 'POST',
-      body:{ text: taskText },
-      headers:{
-        'Content-Type': 'application/json',
-      }
-    }, (responseData)=>{
-      const generatedId = responseData.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    })
+    addNewTask(
+      {
+        url: "https://react-udemy-course-custom-hook-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      createTask.bind(null, taskText)
+    );
   };
 
   return (
