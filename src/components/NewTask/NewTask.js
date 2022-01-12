de251feddb4985025588e5602c64704e0fc35d9c
abrunello <brunello@bludata.com>
2022-01-12 15:42:11 +0100

    using custom hook inside more than one component

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,25 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
+  const { isLoading, error, sendRequest: addNewTask } = useHttp();
+  
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-udemy-course-custom-hook-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
+    addNewTask({
+      url:'https://react-udemy-course-custom-hook-default-rtdb.firebaseio.com/tasks.json',
+      method: 'POST',
+      body:{ text: taskText },
+      headers:{
+        'Content-Type': 'application/json',
       }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
+    }, (responseData)=>{
+      const generatedId = responseData.name; // firebase-specific => "name" contains generated id
       const createdTask = { id: generatedId, text: taskText };
 
       props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    })
   };
 
   return (
